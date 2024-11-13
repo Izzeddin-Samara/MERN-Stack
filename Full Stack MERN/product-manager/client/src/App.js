db8702f6ad554590284bf040130d79bc9fdfe9fe
Izzsamara <izzidinsamara@gmail.com>
2024-11-13 14:38:54 +0200

    Add routing for product pages

--- Full Stack MERN/product-manager/client/src/App.js
+++ Full Stack MERN/product-manager/client/src/App.js
@@ -1,14 +1,21 @@
 import React from 'react';
-import './App.css';
-import ProductForm from './components/AddProduct';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import AllProducts from './components/AllProducts';
+import ProductDetails from './components/ProductDetails';
+import AddProduct from './components/AddProduct';
 
-function App() {
-  return (
-    <div className="App">
-      <h1>Product Form</h1>
-      <ProductForm />
-    </div>
-  );
-}
+const App = () => {
+    return (
+        <Router>
+            <div>
+                <Routes>
+                    <Route path="/products" element={<AllProducts />} />
+                    <Route path="/products/:id" element={<ProductDetails />} />
+                    <Route path="/" element={<AddProduct />} />
+                </Routes>
+            </div>
+        </Router>
+    );
+};
 
 export default App;
