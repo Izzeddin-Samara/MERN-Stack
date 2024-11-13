84af5c2d650901a115d8df3d543fdcade44e59c5
Izzsamara <izzidinsamara@gmail.com>
2024-11-13 14:20:19 +0200

    Add route for fetching a product by ID

--- Full Stack MERN/product-manager/server/routes/productRoutes.js
+++ Full Stack MERN/product-manager/server/routes/productRoutes.js
@@ -5,5 +5,6 @@ const productController = require('../controllers/productController');
 
 router.post('/', productController.createProduct);
 router.get('/', productController.getAllProducts);
+router.get('/:id', productController.getProductById);
 
 module.exports = router;
