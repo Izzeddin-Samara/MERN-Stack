fc98445499e92a6ea92d951bd05dd30c345700b7
Izzsamara <izzidinsamara@gmail.com>
2024-11-13 18:12:06 +0200

    Add route for delete product

--- Full Stack MERN/product-manager/server/routes/productRoutes.js
+++ Full Stack MERN/product-manager/server/routes/productRoutes.js
@@ -7,5 +7,6 @@ router.post('/', productController.createProduct);
 router.get('/', productController.getAllProducts);
 router.get('/:id', productController.getProductById);
 router.put('/update/:id', productController.UpdateProduct);
+router.delete('/delete/:id', productController.DeleteProduct);
 
 module.exports = router;
