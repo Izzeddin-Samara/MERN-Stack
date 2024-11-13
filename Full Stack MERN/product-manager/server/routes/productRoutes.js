4c4b320105ecc114f91549afe98dc98da21789ca
Izzsamara <izzidinsamara@gmail.com>
2024-11-13 18:10:33 +0200

    Add route for update product

--- Full Stack MERN/product-manager/server/routes/productRoutes.js
+++ Full Stack MERN/product-manager/server/routes/productRoutes.js
@@ -6,5 +6,6 @@ const productController = require('../controllers/productController');
 router.post('/', productController.createProduct);
 router.get('/', productController.getAllProducts);
 router.get('/:id', productController.getProductById);
+router.put('/update/:id', productController.UpdateProduct);
 
 module.exports = router;
