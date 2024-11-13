5920257caa900fc43550bfc4d5272685a501d522
Izzsamara <izzidinsamara@gmail.com>
2024-11-13 14:13:30 +0200

    Add getProductById function to fetch a product by ID

--- Full Stack MERN/product-manager/server/controllers/productController.js
+++ Full Stack MERN/product-manager/server/controllers/productController.js
@@ -19,3 +19,16 @@ exports.getAllProducts = async (req, res) => {
   }
 };
 
+
+exports.getProductById = async (req, res) => {
+  try {
+    const product = await Product.findById(req.params.id);
+    if (!product) {
+      return res.status(404).json({ message: 'Product not found' });
+    }
+    res.json(product);
+  } catch (error) {
+    res.status(400).json({ message: error.message });
+  }
+};
+
