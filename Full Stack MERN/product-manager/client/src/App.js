fd9abca89013f45428518e589499dcadcf79e834
Izzsamara <izzidinsamara@gmail.com>
2024-11-13 12:31:17 +0200

    Add all client-side files

---
+++ Full Stack MERN/product-manager/client/src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './App.css';
+import ProductForm from './components/AddProduct';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Product Form</h1>
+      <ProductForm />
+    </div>
+  );
+}
+
+export default App;
---
+++ Full Stack MERN/product-manager/client/src/components/AddProduct.js
@@ -0,0 +1,72 @@
+import React, { useState } from 'react';
+import axios from 'axios';
+
+const AddProduct = () => {
+    const [title, setTitle] = useState('');
+    const [price, setPrice] = useState('');
+    const [description, setDescription] = useState('');
+    const [successMessage, setSuccessMessage] = useState('');
+    const [errorMessage, setErrorMessage] = useState('');
+
+    const handleAddProduct = (e) => {
+        e.preventDefault();
+
+        const productData = {
+            title,
+            price,
+            description
+        };
+
+        axios.post('http://localhost:5000/api/products', productData)
+            .then((response) => {
+                setTitle('');
+                setPrice('');
+                setDescription('');
+                setSuccessMessage('Product added successfully!');
+            })
+            .catch((error) => {
+                setErrorMessage('Failed to add the product. Please try again.');
+            });
+    };
+
+    return (
+        <div>
+            {successMessage && <div>{successMessage}</div>}
+            {errorMessage && <div>{errorMessage}</div>}
+            <form onSubmit={handleAddProduct}>
+                <div>
+                    <input
+                        type="text"
+                        id="title"
+                        value={title}
+                        onChange={(e) => setTitle(e.target.value)}
+                        required
+                        placeholder='Title'
+                    />
+                </div>
+                <div>
+                    <input
+                        type="number"
+                        id="price"
+                        value={price}
+                        onChange={(e) => setPrice(e.target.value)}
+                        required
+                        placeholder='Price'
+                    />
+                </div>
+                <div>
+                    <textarea
+                        id="description"
+                        value={description}
+                        onChange={(e) => setDescription(e.target.value)}
+                        required
+                        placeholder='Description'
+                    />
+                </div>
+                <button type="submit">Add Product</button>
+            </form>
+        </div>
+    );
+};
+
+export default AddProduct;
