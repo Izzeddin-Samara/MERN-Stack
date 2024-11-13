2c88d66ec644b669923114314c8f8cb014ff7a14
Izzsamara <izzidinsamara@gmail.com>
2024-11-13 14:16:00 +0200

    Add route for fetching all products

--- Full Stack MERN/product-manager/server/routes/productRoutes.js
+++ Full Stack MERN/product-manager/server/routes/productRoutes.js
@@ -2,7 +2,8 @@ const express = require('express');
 const router = express.Router();
 const productController = require('../controllers/productController');
 
-// Create Product (POST request)
+
 router.post('/', productController.createProduct);
+router.get('/', productController.getAllProducts);
 
 module.exports = router;
