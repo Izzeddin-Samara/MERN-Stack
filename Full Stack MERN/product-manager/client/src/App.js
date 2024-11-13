2f1d0a7382ba5a20bd54dea0807f19277c31c879
Izzsamara <izzidinsamara@gmail.com>
2024-11-13 18:18:00 +0200

    Add routing for update product page

--- Full Stack MERN/product-manager/client/src/App.js
+++ Full Stack MERN/product-manager/client/src/App.js
@@ -3,6 +3,7 @@ import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import AllProducts from './components/AllProducts';
 import ProductDetails from './components/ProductDetails';
 import AddProduct from './components/AddProduct';
+import UpdateProduct from './components/UpdateProduct';
 
 const App = () => {
     return (
@@ -11,6 +12,7 @@ const App = () => {
                 <Routes>
                     <Route path="/products" element={<AllProducts />} />
                     <Route path="/products/:id" element={<ProductDetails />} />
+                    <Route path="/products/update/:id" element={<UpdateProduct />} />
                     <Route path="/" element={<AddProduct />} />
                 </Routes>
             </div>
