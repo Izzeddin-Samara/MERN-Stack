e1a87b8504f9b6288ea71470cdf4530bb768cdbe
Izzsamara <izzidinsamara@gmail.com>
2024-11-20 12:21:38 +0200

    Refactor App.js: Add routes for managing authors with React Router

--- Full Stack MERN/authors/client/src/App.js
+++ Full Stack MERN/authors/client/src/App.js
@@ -1,25 +1,24 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import AddAuthor from './components/AddAuthor';
+import UpdateAuthor from './components/UpdateAuthor';
+import AuthorDetails from './components/AuthorDetails';
+import AllAuthors from './components/AllAuthors';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+const App = () => {
+    return (
+        <Router>
+            <div>
+                <Routes>
+                    <Route path="/authors" element={<AllAuthors />} />
+                    <Route path="/authors/:id" element={<AuthorDetails />} />
+                    <Route path="/authors/update/:id" element={<UpdateAuthor />} />
+                    <Route path="/authors/new" element={<AddAuthor />} />
+                </Routes>
+            </div>
+        </Router>
+    );
+};
 
 export default App;
